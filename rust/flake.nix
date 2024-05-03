89008f71cc7bdeeb2e8360be75beb0726d664ac8
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-05-03 11:46:40 +0200

    Lock Rust to 1.77.1
    As per https://www.nixhub.io/packages/rustc

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/92d295f588631b0db2da509f381b4fb1e74173c5";
     utils.url = "github:numtide/flake-utils";
   };
 
