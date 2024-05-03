6c9ca79c11085d8de86719f6a34b06b2c72a93eb
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-05-03 15:32:18 +0200

    Add Postgres development environment
    Locked on 15.6 as per https://www.nixhub.io/packages/postgresql.

---
+++ postgresql/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            postgresql
+          ];
+        };
+      }
+    );
+}
