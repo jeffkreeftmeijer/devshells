a22052f0b871adb958f81a58e6682ce2d09b280e
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-05-14 19:55:38 +0200

    Add Rustup flake
    Locked on 1.26.0, as per https://www.nixhub.io/packages/rustup
    
    Basically a copy of the Rust flake, but using Rustup instead of
    separate utilities.

---
+++ rustup/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/92d295f588631b0db2da509f381b4fb1e74173c5";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            darwin.apple_sdk.frameworks.Security
+            libiconv
+            gcc
+            rustup
+          ];
+        };
+      }
+    );
+}
